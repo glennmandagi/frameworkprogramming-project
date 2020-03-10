44a4b22649cc6b1019040fbd80e04110e41749e6
Glenn Mandagi <glennmandagi994@gmail.com>
2020-03-10 20:18:13 +0800

    Update Script2.js

--- Script2.js
+++ Script2.js
@@ -230,3 +230,150 @@
 // }
 // console.log(validateCart('Sabun Cuci', 'Tissue', 'Buah Apel'));
 // console.log(validateCart('Pepsodent', 'Aqua', 'Roti'));
+
+/*
+    6. Destructuring
+*/
+
+// let myArr = ['a', 'b', 'c', 'd'];
+
+// ES5
+// let a5 = myArr[0];
+// let b5 = myArr[1];
+// let c5 = myArr[2];
+// let d5 = myArr[3];
+// console.log(a5, b5, c5, d5);
+
+// ES6
+// let [a6, b6, c6, d6] = myArr;
+// console.log (a6, b6, c6, d6);
+
+// let [a6, ...rest] = myArr;
+// console.log(a6);
+// console.log(rest);
+
+// Swapping variable
+// let a = 5, b = 7;
+// console.log(a, b);
+
+// Swapping code
+// ES5
+// let temp;
+// temp = a;
+// a = b;
+// b = temp;
+
+//ES6
+// [a, b] = [b, a];
+// console.log(a, b);
+
+// const func = () => [1, 2, 3]
+// const [a, b, c] = func();
+// console.log(a, b, c);
+
+// let myObj = {
+//     one: 'a',
+//     two: 'b',
+//     three: 'c',
+//     four: () => 'd'
+// }
+
+// ES5
+// let one5 = myObj.one
+// let two5 = myObj.two
+// let three5 = myObj.three
+// let four5 = myObj.four();
+// myObj.four();
+// console.log(one5, two5, three5, four5);
+
+// ES6
+// let { one: one6, two: two6, three: three6, four: four6 } = myObj;
+// console.log(one6, two6, three6, four6);
+
+// let myObj = {
+//     one: 'a',
+//     two: 'b',
+//     three: 'c',
+//     four: () => 'd'
+// }
+
+// let { one, ...rest} = myObj;
+// console.log(one);
+// console.log(rest);
+
+// const addNumber = ({num1 = 10, num2, num3}) => {
+//     return num1 + num2 + num3;
+// }
+
+// console.log(addNumber({ num2: 15, num3: 25 }));
+
+// /*
+//     7. Classes
+// */
+
+// class Person {
+//     constructor(gender) {
+//         this.gender = gender;
+//     }
+// }
+
+// class Student extends Person {
+//     constructor(gender, firstName, lastName, score) {
+//         super(gender);
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.score = score;
+//     }
+
+//     displayFullName(){
+//         return `Hello ${this.firstName} ${this.lastName}. Your gender is ${this.gender}`;
+//     }
+// }
+
+// const student1 = new Student('Male', 'John', 'Smith', 90);
+// console.log(student1.displayFullName());
+// const student2 = new Student('Female', 'Mary', 'Miller', 80);
+// console.log(student2.displayFullName());
+
+//                             Vehicle
+// Car (model, year, engine)        // Truck (model, year, engine)
+// Toyota        Nissan             Hino            Mitsubishi
+
+// class Vehicle {
+//     constructor(type){
+//         this.type = type;
+//     }
+// }
+
+// class Car extends Vehicle {
+//     constructor(model, year, engine){
+//         super(model);
+//         this.model = model;
+//         this.year = year;
+//         this.engine = engine;
+//     }
+
+//     displayCarName(){
+//         return `Hello ${this.model} made in ${this.year}. The engine is ${this.engine}`;
+//     }
+// }
+
+// class Truck extends Vehicle {
+//     constructor(model, year, engine) {
+//         super(model);
+//         this.model = model;
+//         this.year = year;
+//         this.engine = engine;
+//     }
+
+//     displayTruckName(){
+//         return `Hello ${this.model} made in ${this.year}. The engine is ${this.engine}`;
+//     }
+// }
+
+// const car1 = new Car('Toyota Agya', 2015, '200cc');
+// console.log(car1.displayCarName());
+// const car2 = new Car('Nissan Terrano', 2015, '200cc');
+// console.log(car2.displayCarName());
+// const truck1 = new Truck('Hino Dutro', 2006, '275 cc');
+// console.log(truck1.displayTruckName());
